df41dca835e825245eee962d431fe6b2765bd6f0
khueniken <khueniken@gmail.com>
2024-10-15 12:20:35 -0400

    Update as_numeric_parse()

--- R/as_numeric_parse.R
+++ R/as_numeric_parse.R
@@ -2,7 +2,7 @@
 #'
 #' This function is a wrapper for as.numeric(). It takes a vector as input and
 #' returns a numeric vector, while printing to the console every entry that
-#' cannot be coerced. Editing this file for fetch. 
+#' cannot be coerced.
 #' @param x a string or vector to coerce to numeric
 #' @param ... additional arguments passed to `as.numeric()
 #' @keywords KHtools
@@ -15,14 +15,14 @@ as_numeric_parse <- function(x, ...){
   if (!class(x) %in% c("logical","numeric","double","integer","character","Date")) {
     stop("Invalid data type")
   }
-  
+
   y <- suppressWarnings(as.numeric(x, ...))
   noparse <- x[!is.na(x) & is.na(y)]
-  
+
   if (length(noparse) > 0) {
     noparse_warn <- paste0("Entry ", which(!is.na(x) & is.na(y)), ", '", noparse, "'")
     message("The following entries were converted to NA values:")
-    
+
     for (i in 1:length(noparse_warn)) message(noparse_warn[i])
   }
   return(y)
